2d76cc25bda52095fb54ad2ff0f8d216fc554512
Robin Varghese <robinv312@gmail.com>
2020-10-24 22:28:23 +0530

    commiting the latest changes

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import TextError from './TextError'
 
 const initialValues = {
     name: '',
     email: '',
-    channel: ''
+    channel: '',
+    comments: '',
+    address: '',
+    social: {
+        facebook: '',
+        twitter: ''
+    },
+    phoneNumbers: ['', '']
 }
 
 const onSubmit = (values) => {
@@ -52,7 +60,7 @@ function YouTubeForm() {
                     // onBlur={formik.handleBlur} 
                     // {...formik.getFieldProps('name')}
                     />
-                    <ErrorMessage name="name"/>
+                    <ErrorMessage name="name" component={TextError} />
                 </div>
 
                 <div className="form-control">
@@ -60,16 +68,62 @@ function YouTubeForm() {
                     <Field type="text" id="email" name="email"
                     // {...formik.getFieldProps('email')}
                     />
-                    <ErrorMessage name="email" />
+                    <ErrorMessage name="email" >
+                        {
+                            (errorMsg) => <div className='error'>{errorMsg}</div>
+                        }
+                    </ErrorMessage>
                 </div>
 
                 <div className="form-control">
                     <label htmlFor='channel'>Channel</label>
                     <Field type="text" id="channel" name="channel"
+                        placeholder="Youtube channel name"
                     // {...formik.getFieldProps('channel')}
                     />
-                    <ErrorMessage name="channel"/>
+                    <ErrorMessage name="channel" />
                 </div>
+                <div className='form-control'>
+                    <label htmlFor='comments'>Comments</label>
+                    <Field as='textarea' id='comments' name='comments' />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='address'>Address</label>
+                    <Field name='address' >
+                        {(props) => {
+                            const { field, form, meta } = props
+                            return (<div><input type='text' id='address' {...field} />
+                                {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                            </div>)
+                        }}
+                    </Field>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='facebook'>Facebook</label>
+                    <Field type='text' id='facebook' name='social.facebook' />
+
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='twitter'>Twitter</label>
+                    <Field type='text' id='twitter' name='social.twitter' />
+                </div>
+
+
+                <div className='form-control'>
+                    <label htmlFor='primaryPh'>Primary Phone Number</label>
+                    <Field type='text' id='primaryPh' name='primaryPh[0]' />
+
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='secondaryPh'>Secondary Phone Number</label>
+                    <Field type='text' id='secondaryPh' name='primaryPh[1]' />
+                </div>
+
+
+
+
+
+
 
 
                 <button type="submit" >Submit</button>
